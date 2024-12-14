82ba95452fa682c088e41074ca7be2a7fade88f8
jvidalv <josepvidalvidal@gmail.com>
2024-12-14 12:42:11 +0100

    feat: init

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,7 +1,6 @@
 import { Elysia } from "elysia";
-import { hello } from "@/api/routes/hello.route";
-import { pokemons } from "@/api/routes/pokemon.route";
+import { mountainsRoute } from "@/api/routes/mountains.route";
 
-export const app = new Elysia({ prefix: "/api" }).use(pokemons).use(hello);
+export const app = new Elysia({ prefix: "/api" }).use(mountainsRoute);
 
 export type App = typeof app;
--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -13,7 +13,7 @@ const MAPPING = {
   "paperplane.fill": "send",
   "chevron.left.forwardslash.chevron.right": "code",
   "chevron.right": "chevron-right",
-  cloud: "cloud",
+  "square.and.arrow.up": "share",
 } as Partial<
   Record<
     import("expo-symbols").SymbolViewProps["name"],
