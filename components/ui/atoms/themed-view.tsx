06dc2aaadc476223ae55787186b40ff934f3d3f1
jvidalv <josepvidalvidal@gmail.com>
2024-12-27 10:03:01 +0100

    feat: add themed core components

--- components/ui/atoms/themed-view.tsx
+++ components/ui/atoms/themed-view.tsx
@@ -1,7 +1,6 @@
 import { View, type ViewProps } from "react-native";
-
-import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function ThemedView({ className, ...props }: ViewProps) {
-  return <View className={clsx("bg-background", className)} {...props} />;
+  return <View className={twMerge("bg-background", className)} {...props} />;
 }
