8ea1f4a721150617a71be2112353107043632b6b
jvidalv <josepvidalvidal@gmail.com>
2024-12-31 17:38:19 +0100

    feat: extend eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,31 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
+  extends: ["expo", "prettier", "plugin:tailwindcss/recommended"],
   ignorePatterns: ["/dist/*"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "react-hooks", "import", "tailwindcss"],
   rules: {
     "prettier/prettier": "error",
+    "@typescript-eslint/no-unused-vars": ["error", {}],
+    "no-empty-pattern": "error",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          ["builtin"], // Node.js built-ins
+          ["external"], // External packages
+          ["internal"], // Internal modules
+          ["parent", "sibling", "index"], // Parent, sibling, and index modules
+          ["object"], // Object imports (e.g., `import * as X`)
+          ["type"], // Type imports
+          ["unknown"], // Unknown imports (e.g., side effects)
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc", // Sort imports alphabetically
+          caseInsensitive: true, // Ignore case while sorting
+        },
+        "newlines-between": "always", // Add a newline between groups
+      },
+    ],
   },
 };
