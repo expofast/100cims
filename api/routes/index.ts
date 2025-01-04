73e509ff4de25b6b642d4ac52579ea7bf8330014
jvidalv <josepvidalvidal@gmail.com>
2025-01-04 13:56:29 +0100

    feat: remove after handle

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,15 +1,15 @@
 import { cors } from "@elysiajs/cors";
 import { Elysia } from "elysia";
 
-import { postgresClient } from "@/api/db";
+// import { postgresClient } from "@/api/db";
 import { protectedRoutes } from "@/api/routes/protected";
 import { publicRoutes } from "@/api/routes/public";
 
 export const app = new Elysia({ prefix: "/api" })
   .use(cors())
-  .onAfterHandle(() => {
-    void postgresClient.end();
-  })
+  // .onAfterHandle(() => {
+  //   void postgresClient.end();
+  // })
   .use(publicRoutes)
   .use(protectedRoutes);
 
