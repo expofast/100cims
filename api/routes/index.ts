9fa0fce0d02b07524d43f5384f675da8ae505bd6
jvidalv <josepvidalvidal@gmail.com>
2025-01-28 17:49:45 +0100

    feat: handle errors on the top level

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,11 +1,33 @@
 import { cors } from "@elysiajs/cors";
-import { Elysia } from "elysia";
+import { Elysia, ParseError, ValidationError } from "elysia";
 
+import { addRowToSheets, ERRORS_SPREADSHEET } from "@/api/lib/sheets";
 import { protectedRoutes } from "@/api/routes/protected";
 import { publicRoutes } from "@/api/routes/public";
 
 export const app = new Elysia({ prefix: "/api" })
   .use(cors())
+  .onError(({ request, error }) => {
+    if (error instanceof ValidationError) {
+      void addRowToSheets(ERRORS_SPREADSHEET, [
+        "validation",
+        error.status,
+        request.url,
+      ]);
+    } else if (error instanceof ParseError) {
+      void addRowToSheets(ERRORS_SPREADSHEET, [
+        "parse",
+        error.status,
+        request.url,
+      ]);
+    } else {
+      void addRowToSheets(ERRORS_SPREADSHEET, [
+        "generic",
+        JSON.stringify(error),
+        request.url,
+      ]);
+    }
+  })
   .use(publicRoutes)
   .use(protectedRoutes);
 
