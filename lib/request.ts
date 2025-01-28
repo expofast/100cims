40018a7a4f7b30c0e1104bca30fd6228955484a4
jvidalv <josepvidalvidal@gmail.com>
2025-01-28 17:28:59 +0100

    chore: refactor api usage

--- lib/request.ts
+++ lib/request.ts
@@ -1,16 +1,11 @@
 import { treaty } from "@elysiajs/eden";
+import { Treaty } from "@elysiajs/eden/dist/treaty2";
 
 import { App } from "@/api/routes";
-import { getJwt } from "@/lib/auth";
 
-const app = treaty<App>(process.env.EXPO_PUBLIC_API_URL || "", {
-  headers: (path) => {
-    if (path.startsWith("/api/protected")) {
-      return [{ Authorization: `Bearer ${getJwt()}` }];
-    }
+export const request = (config?: Treaty.Config) =>
+  treaty<App>(process.env.EXPO_PUBLIC_API_URL || "", {
+    ...config,
+  });
 
-    return {};
-  },
-});
-
-export const api = app.api;
+export const api = request().api;
