18afae9beca0df9021c2091493b1637dc5b188cf
Josep Vidal <josepvidalvidal@gmail.com>
2025-05-04 12:52:07 +0200

    feat: refresh summits view

--- app/summits.tsx
+++ app/summits.tsx
@@ -1,90 +1,136 @@
 import { format } from "date-fns/format";
-import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
-import { FormattedMessage, useIntl } from "react-intl";
-import { View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { FormattedMessage } from "react-intl";
+import { View, TouchableOpacity, Image, ScrollView } from "react-native";
+import { twMerge } from "tailwind-merge";
 
-import { useChallenge } from "@/components/providers/challenge-provider";
-import { Skeleton, ThemedText } from "@/components/ui/atoms";
-import { AvatarGroup } from "@/components/ui/molecules";
-import ParallaxScrollView from "@/components/ui/organisms/parallax-scroll-view";
-import { useChallengesGet } from "@/domains/challenge/challenge.api";
+import { Skeleton, ThemedText, ThemedView } from "@/components/ui/atoms";
+import { AvatarGroup, ScreenHeader } from "@/components/ui/molecules";
 import { useSummitsGet } from "@/domains/summit/summit.api";
 import { getFullName } from "@/domains/user/user.utils";
 
 export default function SummitsScreen() {
   const router = useRouter();
-  const intl = useIntl();
-  const { challengeId } = useChallenge();
-  const { data: challenges } = useChallengesGet();
-  const challenge = challenges?.find(
-    (challenge) => challenge.id === challengeId,
-  );
   const { data: summits, isPending: isPendingSummits } = useSummitsGet({
     limit: 100,
   });
 
   return (
-    <ParallaxScrollView
-      title={challenge?.name || "..."}
-      subtitle={intl.formatMessage({ defaultMessage: "Challenge" })}
-      headerClassName="flex items-center justify-center bg-primary"
-      contentClassName="py-6"
-      headerImage={<View className="flex-1 bg-primary" />}
-      height={200}
-    >
-      <ThemedText className="mb-4 px-6 text-2xl font-semibold">
-        <FormattedMessage defaultMessage="Last summits" />
-      </ThemedText>
-      <View className="flex flex-row flex-wrap">
+    <ThemedView className="flex-1">
+      <ScreenHeader />
+      <ScrollView contentContainerClassName="flex flex-row flex-wrap mx-4">
+        <ThemedText className="mx-1 mb-4 text-4xl font-bold">
+          <FormattedMessage defaultMessage="Latest summits" />
+        </ThemedText>
         {isPendingSummits && (
           <>
-            <Skeleton className="h-44 w-1/2 rounded-none" />
-            <Skeleton className="h-44 w-1/2 rounded-none border-l border-background" />
-            <Skeleton className="h-44 w-1/2 rounded-none border-t border-background" />
-            <Skeleton className="h-44 w-1/2 rounded-none border-l border-t border-background" />
-            <Skeleton className="h-44 w-1/2 rounded-none border-t border-background" />
-            <Skeleton className="h-44 w-1/2 rounded-none border-l border-t border-background" />
-            <Skeleton className="h-44 w-1/2 rounded-none border-t border-background" />
-            <Skeleton className="h-44 w-1/2 rounded-none border-l border-t border-background" />
+            <View className="w-1/2">
+              <Skeleton
+                className="w-full border-background mb-2"
+                style={{ height: 243, borderRadius: 6 }}
+              />
+            </View>
+            <View className="w-1/2 pl-1.5">
+              <Skeleton
+                className="w-full border-background mb-2"
+                style={{ height: 243, borderRadius: 6 }}
+              />
+            </View>
+            <View className="w-1/2">
+              <Skeleton
+                className="w-full border-background mb-2"
+                style={{ height: 243, borderRadius: 6 }}
+              />
+            </View>
+            <View className="w-1/2 pl-1.5">
+              <Skeleton
+                className="w-full border-background mb-2"
+                style={{ height: 243, borderRadius: 6 }}
+              />
+            </View>
+            <View className="w-1/2">
+              <Skeleton
+                className="w-full border-background mb-2"
+                style={{ height: 243, borderRadius: 6 }}
+              />
+            </View>
+            <View className="w-1/2 pl-1.5">
+              <Skeleton
+                className="w-full border-background mb-2"
+                style={{ height: 243, borderRadius: 6 }}
+              />
+            </View>
+            <View className="w-1/2">
+              <Skeleton
+                className="w-full border-background mb-2"
+                style={{ height: 243, borderRadius: 6 }}
+              />
+            </View>
+            <View className="w-1/2 pl-1.5">
+              <Skeleton
+                className="w-full border-background mb-2"
+                style={{ height: 243, borderRadius: 6 }}
+              />
+            </View>
           </>
         )}
-        {summits?.map(({ summitId, summitImageUrl, summitedAt, users }) => (
-          <TouchableOpacity
-            key={summitId}
-            onPress={() =>
-              router.push({
-                pathname: "/user/summits/[summit]",
-                params: { summit: summitId },
-              })
-            }
-            className="relative h-44 w-1/2"
-          >
-            <Image
-              source={{ uri: summitImageUrl }}
-              style={{ height: "100%", width: "100%" }}
-            />
-            <View className="absolute size-full">
-              <LinearGradient
-                colors={["transparent", "transparent", "rgba(0,0,0,0.3)"]}
-                style={StyleSheet.absoluteFill}
-              />
-            </View>
-            <View className="absolute bottom-4 w-full flex-row items-end justify-between px-2">
-              <AvatarGroup
-                size="sm"
-                items={users?.map((participant) => ({
-                  name: getFullName(participant),
-                  imageUrl: participant.imageUrl,
-                }))}
-              />
-              <ThemedText className="font-medium text-white">
-                {format(summitedAt, "dd MMM yyyy")}
-              </ThemedText>
-            </View>
-          </TouchableOpacity>
-        ))}
-      </View>
-    </ParallaxScrollView>
+        {summits?.map(
+          (
+            { summitId, mountainName, summitImageUrl, summitedAt, users },
+            index,
+          ) => (
+            <TouchableOpacity
+              key={summitId}
+              onPress={() =>
+                router.push({
+                  pathname: "/user/summits/[summit]",
+                  params: { summit: summitId },
+                })
+              }
+              className={twMerge(
+                "relative w-1/2 mb-2",
+                index % 2 !== 0 && "pl-1.5",
+              )}
+            >
+              <View className="relative">
+                <Image
+                  source={{ uri: summitImageUrl }}
+                  className="bg-neutral-200 dark:bg-neutral-800"
+                  style={{
+                    height: 200,
+                    width: "100%",
+                    borderTopRightRadius: 6,
+                    borderTopLeftRadius: 6,
+                  }}
+                />
+                <View className="absolute bottom-2 left-2">
+                  <AvatarGroup
+                    size="xs"
+                    avatarClassName="border-background/50"
+                    items={users?.map((participant) => ({
+                      name: getFullName(participant),
+                      imageUrl: participant.imageUrl,
+                    }))}
+                  />
+                </View>
+              </View>
+
+              <View className="w-full border border-t-0 border-gray-200 rounded-b-lg dark:border-0 dark:bg-neutral-800 p-2">
+                <View className="mb-0.5">
+                  <ThemedText numberOfLines={1} className="text-sm font-medium">
+                    {mountainName}
+                  </ThemedText>
+                </View>
+                <View className="flex-row items-center justify-between gap-2">
+                  <ThemedText className="text-muted-foreground text-xs">
+                    {format(summitedAt, "dd MMM yyyy")}
+                  </ThemedText>
+                </View>
+              </View>
+            </TouchableOpacity>
+          ),
+        )}
+      </ScrollView>
+    </ThemedView>
   );
 }
